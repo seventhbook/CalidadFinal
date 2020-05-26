6c0d7470d7b1a6f4ef115bc88bc1cc652636373a
ToxiFranko <65483277+ToxiFranko@users.noreply.github.com>
2020-05-26 17:29:17 -0500

    Update contract.php

--- htdocs/admin/contract.php
+++ htdocs/admin/contract.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2011-2013      Juanjo Menent	    <jmenent@2byte.es>
  * Copyright (C) 2011-2018      Philippe Grand	    <philippe.grand@atoo-net.com>
- *
+ 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
