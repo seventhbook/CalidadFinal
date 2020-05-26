c8278a8c2291697a34adc86f35490aa321af8c55
ToxiFranko <65483277+ToxiFranko@users.noreply.github.com>
2020-05-26 14:36:09 -0500

    Update paypal.lib.php

--- htdocs/paypal/lib/paypal.lib.php
+++ htdocs/paypal/lib/paypal.lib.php
@@ -630,8 +630,8 @@ function hash_call($methodName, $nvpStr)
 
     //turning off the server and peer verification(TrustManager Concept).
    
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, TRUE);
 
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, empty($conf->global->MAIN_USE_CONNECT_TIMEOUT) ? 5 : $conf->global->MAIN_USE_CONNECT_TIMEOUT);
     curl_setopt($ch, CURLOPT_TIMEOUT, empty($conf->global->MAIN_USE_RESPONSE_TIMEOUT) ? 30 : $conf->global->MAIN_USE_RESPONSE_TIMEOUT);
