3dd83bb5608eac3085aa2f88d60d6445475dd6bb
antoniofd20 <61999862+antoniofd20@users.noreply.github.com>
2020-05-26 17:57:05 -0500

    Update task.php
    Quite comentarios

--- htdocs/projet/tasks/task.php
+++ htdocs/projet/tasks/task.php
@@ -311,7 +311,6 @@ if ($id > 0 || !empty($ref))
 
             // Other attributes
             $cols = 2;
-            //include DOL_DOCUMENT_ROOT . '/core/tpl/extrafields_view.tpl.php';
 
             print '</table>';
 
@@ -347,34 +346,6 @@ if ($id > 0 || !empty($ref))
 
 			print '<br>';
 		}
-
-		/*
-		 * Actions
-		*/
-		/*print '<div class="tabsAction">';
-
-		if ($user->rights->projet->all->creer || $user->rights->projet->creer)
-		{
-		if ($projectstatic->public || $userWrite > 0)
-		{
-		print '<a class="butAction" href="'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&action=create'.$param.'">'.$langs->trans('AddTask').'</a>';
-		}
-		else
-		{
-		print '<a class="butActionRefused classfortooltip" href="#" title="'.$langs->trans("NotOwnerOfProject").'">'.$langs->trans('AddTask').'</a>';
-		}
-		}
-		else
-		{
-		print '<a class="butActionRefused classfortooltip" href="#" title="'.$langs->trans("NotEnoughPermissions").'">'.$langs->trans('AddTask').'</a>';
-		}
-
-		print '</div>';
-		*/
-
-		// To verify role of users
-		//$userAccess = $projectstatic->restrictedProjectArea($user); // We allow task affected to user even if a not allowed project
-		//$arrayofuseridoftask=$object->getListContactId('internal');
 
 		$head = task_prepare_head($object);
 
