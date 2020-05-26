caffd3e70283e817e2f907eac361db538536b3b3
ToxiFranko <65483277+ToxiFranko@users.noreply.github.com>
2020-05-26 13:03:16 -0500

    Update tcpdf_static.php

--- htdocs/includes/tecnickcom/tcpdf/include/tcpdf_static.php
+++ htdocs/includes/tecnickcom/tcpdf/include/tcpdf_static.php
@@ -1849,8 +1849,8 @@ class TCPDF_STATIC {
 		}
 		curl_setopt($crs, CURLOPT_CONNECTTIMEOUT, 5);
 		curl_setopt($crs, CURLOPT_TIMEOUT, 30);
-		curl_setopt($crs, CURLOPT_SSL_VERIFYPEER, true);
-		curl_setopt($crs, CURLOPT_SSL_VERIFYHOST, true);
+		curl_setopt($crs, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt($crs, CURLOPT_SSL_VERIFYHOST, 1);
 		curl_setopt($crs, CURLOPT_USERAGENT, 'tc-lib-file');
 		curl_exec($crs);
 		$code = curl_getinfo($crs, CURLINFO_HTTP_CODE);
